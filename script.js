f7a27876aba07763f0a3a82b58b43556bed8105d
Sonu Kumar <128314830+sonukds@users.noreply.github.com>
2024-07-31 21:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+// Get modal element
+var modal = document.getElementById("myModal");
+
+// Get open modal button
+var openModalBtn = document.getElementById("openModal");
+
+// Get close button
+var closeModalBtn = document.getElementsByClassName("close-modal")[0];
+
+// Open modal on button click
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Close modal on close button click
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside of the modal content
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
